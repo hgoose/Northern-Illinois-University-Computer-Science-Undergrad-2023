c1e0804789ccfc651c146e97f3f130177d63f3c8
hgoose <noodlecloudcode@gmail.com>
2023-11-25 13:56:29 -0600

    Commit

--- selfstudy/CS/cpp/projects/workflow/main.cpp
+++ selfstudy/CS/cpp/projects/workflow/main.cpp
@@ -1,27 +1,55 @@
 #include <QApplication>
+#include <QString>
 #include <QWidget>
 #include <QPushButton>
 #include <QLabel>
 #include <QProcess>
 #include <iostream>
+#include <cstdint>
+#include <string>
+
+using std::cout;
+using std::string;
+using std::vector;
 
 class MainWindow : public QWidget {
 private:
     Q_OBJECT;
-    QPushButton* button;
+    vector<QPushButton*> buttons;
     QLabel* label;
 
 public:
     MainWindow(QWidget* parent=nullptr) : QWidget(parent) {
-        button = new QPushButton("Click", this);
-        button->setGeometry(50,50,200,50);
-        button->show();
-        connect(button,&QPushButton::clicked, this, &MainWindow::onClick);
 
-        label = new QLabel("Hello world", this);
+        for (int i=0; i<3; ++i) {
+            QString a = "Button " + QString::number(i+1);
+            makeButton(a);
+        }
+
+        displayButtons();
+        // button->setGeometry(50,50,200,50);
+        // button->show();
+        // connect(button,&QPushButton::clicked, this, &MainWindow::onClick);
+
+        label = new QLabel("mane world", this);
         label->setAlignment(Qt::AlignCenter);
         label->setGeometry(50,50,250,50);
         label->hide();
+    }
+
+    void makeButton(const QString& textDisplay) {
+        QPushButton* button;
+        button = new QPushButton(textDisplay, this);
+        button->hide();
+        buttons.push_back(button);
+    }
+
+    void displayButtons() { 
+        uint32_t x{50}, y{50};
+        for (std::vector<QPushButton*>::iterator it=buttons.begin(); it!=buttons.end(); ++it, y+=150 ) {
+            (*it)->setGeometry(x,y,200,150);
+            (*it)->show();
+        }
     }
 
 public slots:
