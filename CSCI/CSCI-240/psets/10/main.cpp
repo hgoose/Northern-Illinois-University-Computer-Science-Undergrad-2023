2f7e10b7a344e2132bedad1e13e816a84aca3d83
hgoose <noodlecloudcode@gmail.com>
2023-12-05 11:41:18 -0600

    Commit

--- CSCI/CSCI-240/psets/10/main.cpp
+++ CSCI/CSCI-240/psets/10/main.cpp
@@ -48,7 +48,6 @@ const char* HEADS[N_HEAD] =  {
     "Squat", "Bench", "DL", 
     "Total", "W. Score"
 };
-
 const char TITLE_DEFAULT[] = "Default List of Powerlifters";
 const char TITLE_TOTAL[] = "Powerlifters Ranked by Total";
 const char TITLE_WILKS[] = "Powerlifters Ranked by Wilks";
@@ -56,11 +55,8 @@ const char TITLE_SQUAT[] = "Powerlifters Ranked by Squat";
 const char TITLE_BENCH[] = "Powerlifters Ranked by Bench";
 const char TITLE_DEADLIFT[] = "Powerlifters Ranked by Deadlift";
 
-/*<remark> 
-The Wilks coefficient is a method by which the strength of a powerlifter is measured against other powerlifters of a different weight category.
-</remark>*/
-
 class PowerLifter {
+    // Private Members:
     char fname[NAME_MAXSIZE];
     char lname[NAME_MAXSIZE];
     float wilks;
@@ -68,6 +64,7 @@ class PowerLifter {
     int maxbench;
     int maxdeadlift;
 
+    // Public method declarations
 public:
     PowerLifter(); 
     void setFirst(const char[]);
@@ -86,17 +83,20 @@ public:
 
 };
 
+// Function Declarations
 void sortBy(PowerLifter[], int, const char[]);
 void swap( PowerLifter&, PowerLifter&);
 int readFile(PowerLifter[], const char[]);
 void printPowerLifters(PowerLifter[], int, const char[]);
 
+// Entry
 int main(int argc, const char *argv[]) {
 
-
+    // Create and build our array
     PowerLifter par[30];
     int count = readFile(par, "lifters.txt");
 
+    // Use the functions and methods
     PROC_COMPLETE(count)
     printPowerLifters(par, count, TITLE_DEFAULT);
     sortBy(par,count,"Total");
@@ -110,74 +110,260 @@ int main(int argc, const char *argv[]) {
     sortBy(par,count,"Deadlift");
     printPowerLifters(par, count, TITLE_DEADLIFT);
     
-
+    // Exit
     return EXIT_SUCCESS;
 }
 
+// Function I
+/***************************************************************
+Function: PowerLifter default constructor
+
+Use: Create powerlifter objects with default values
+
+Arguments: None
+
+Returns: None
+
+Notes: 
+***************************************************************/
 PowerLifter::PowerLifter() : wilks(1), maxsquat(-1), maxbench(-1), maxdeadlift(-1) {
+
+    // Create two C-Strings for default first and last name
     const char* fnametmp = "no";
     const char* lnametmp = "name";
+
+    // Copy those default values into data members for first and last name
     strncpy(this->fname, fnametmp, NAME_MAXSIZE);
     strncpy(this->lname, lnametmp, NAME_MAXSIZE);
 }
 
+// Function II
+/***************************************************************
+Function: SetFirst
+
+Use: Sets the first name of the powerlifter object
+
+Arguments: C-string for first name
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setFirst(const char newFirst[]) {
+    // We just copy the passed in c-string to our data member
     strncpy(this->fname, newFirst, NAME_MAXSIZE);
 }
 
+// Function III
+/***************************************************************
+Function: setLast
+
+Use: Sets the last name of the powerlifter object
+
+Arguments: C-string for last name
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setLast(const char newLast[]) {
+
+    // We just copy the passed in c-string to our data member
     strncpy(this->lname, newLast, NAME_MAXSIZE);
 }
 
+// Function IV
+/***************************************************************
+Function: getSquat
+
+Use: Returns the data member for the max squat
+
+Arguments: None
+
+Returns: Integer which holds the value of the squat data member
+
+Notes: 
+***************************************************************/
 int PowerLifter::getSquat() {
     return this->maxsquat;
 }
 
+// Function V
+/***************************************************************
+Function: setSquat
+
+Use: Sets the data member for max squat
+
+Arguments: integer value for max squat value
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setSquat(int newSquat) {
+
+    // Here we make sure that the new max squat value is valid (at least zero), then we set the data member
     if ( newSquat > -1 ) {
         this->maxsquat = newSquat;
     } 
 }
 
+// Function VI
+/***************************************************************
+Function: getBench
+
+Use: Returns the data member for the max bench
+
+Arguments: None
+
+Returns: Integer which holds the value of the bench data member
+
+Notes: 
+***************************************************************/
 int PowerLifter::getBench() {
     return this->maxbench;
 }
 
+// Function VII
+/***************************************************************
+Function: setBench
+
+Use: Sets the data member for max bench
+
+Arguments: integer value for max bench value
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setBench(int newBench) {
+
+    // Here we make sure that the new max bench value is valid (at least zero), then we set the data member
     if ( newBench > -1 ) {
         this->maxbench = newBench;
     }
 }
 
+// Function VIII
+/***************************************************************
+Function: getDeadlift
+
+Use: Returns the data member for the max deadlift
+
+Arguments: None
+
+Returns: Integer which holds the value of the deadlift data member
+
+Notes: 
+***************************************************************/
 int PowerLifter::getDeadlift() {
     return this->maxdeadlift;
 }
 
+// Function IX
+/***************************************************************
+Function: setDeadlift
+
+Use: Sets the data member for max deadlift
+
+Arguments: integer value for max deadlift value
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setDeadlift(int newDeadlift) {
     if ( newDeadlift > -1 ) {
         this->maxdeadlift = newDeadlift;
     }
 }
 
+// Function X
+/***************************************************************
+Function: getTotal
+
+Use: Returns the summation of the three maxes
+
+Arguments: None
+
+Returns: Integer for the sum of the three movements
+
+Notes: 
+***************************************************************/
 int PowerLifter::getTotal() {
     return this->maxsquat + this->maxbench + this->maxdeadlift;
 }
 
+
+// Function XI
+/***************************************************************
+Function: setCoef
+
+Use: Sets the data member for the wilks coefficient
+
+Arguments: Float for the new wilks coefficient
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::setCoef(float newCoef) {
     if ( newCoef >= 0 && newCoef < 2) {
         this->wilks = newCoef;
     }
-
 }
 
+// Function XII
+/***************************************************************
+Function: wilksScore
+
+Use: Computes the value of the wilksScore
+
+Arguments: None
+
+Returns: Integer for the wilks score
+
+Notes: 
+***************************************************************/
 int PowerLifter::wilksScore() {
+    
+    // Here we just multiply the total by the wilks coefficient then cast the result to an integer
     return static_cast<int>(this->getTotal() * this->wilks);
 }
 
+// Function XIII
+/***************************************************************
+Function: compare
+
+Use: compares the wilks score between two objects
+
+Arguments: Reference to some other object
+
+Returns: Integer: 
+        0: If they are the same
+        -1: if a < b 
+        1: if a > b
+
+Notes: 
+***************************************************************/
 int PowerLifter::compare(PowerLifter& other) {
+
+    // Simple double ternary to deduce which value to return
     return (this->wilks == other.wilks ? 0 : (this->wilks < other.wilks ? -1 : 1));
 }
 
+// Function XIV
+/***************************************************************
+Function: display
+
+Use: Displays formatted object data members
+
+Arguments: None
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void PowerLifter::display() {
     cout << std::setw(STRING_JUST) << this->fname 
         << std::setw(STRING_JUST) << this->lname 
@@ -189,92 +375,124 @@ void PowerLifter::display() {
         << std::setw(NUMERIC_JUST) << this->wilksScore() << endl;
 }
 
+// Function XV
+/***************************************************************
+Function: sortBy
 
+Use: Sorts the objects in our object array based on each attribute (selection sort)
+
+Arguments: obj array, size of array, type to sort by
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void sortBy(PowerLifter array[], int size, const char type[]) {
-    // Valid types 
 
-    /*
-    Squat
-    Bench
-    Deadlift
-    Total
-    Wilks 
-    */
-
-    int min;
-    if (!strcmp(type, "Squat")) {
+    // Logic for each sort is the same, only difference is the attribute to sort by
+    int max;
+    if (!strcmp(type, "Squat")) { // Strcmp to make sure we are sorting by the correct attribute
+    // Logic: Double for loop to check item at index j against the rest of the array, if we find a value smallar than the one at idx j, we take note with the max variable
     for (int j=0; j<size; ++j) {
-        min = j;
+        max = j; // Assume the value at position j is the max
         for (int k=j+1; k<size; ++k) {
-            if (array[k].getSquat() > array[min].getSquat()) {
-                min = k;
+            if (array[k].getSquat() > array[max].getSquat()) { // Comparison
+                max = k;
             }
         }
-        swap(array[j], array[min]);
+        swap(array[j], array[max]); // After the outer loop has completed its iteration and found the lesser value, we perform the swap
     }
     } else if (!strcmp(type, "Bench")) {
         for (int j=0; j<size; ++j) {
-            min = j;
+            max = j;
             for (int k=j+1; k<size; ++k) {
-                if (array[k].getBench() > array[min].getBench()) {
-                    min = k;
+                if (array[k].getBench() > array[max].getBench()) {
+                    max = k;
                 }
             }
-            swap(array[j], array[min]);
+            swap(array[j], array[max]);
         }
     } else if (!strcmp(type, "Deadlift")) {
         for (int j=0; j<size; ++j) {
-            min = j;
+            max = j;
             for (int k=j+1; k<size; ++k) {
-                if (array[k].getDeadlift() > array[min].getDeadlift()) {
-                    min = k;
+                if (array[k].getDeadlift() > array[max].getDeadlift()) {
+                    max = k;
                 }
             }
-            swap(array[j], array[min]);
+            swap(array[j], array[max]);
         }
     } else if (!strcmp(type, "Total")) {
         for (int j=0; j<size; ++j) {
-            min = j;
+            max = j;
             for (int k=j+1; k<size; ++k) {
-                if (array[k].getTotal() > array[min].getTotal()) {
-                    min = k;
+                if (array[k].getTotal() > array[max].getTotal()) {
+                    max = k;
                 }
             }
-            swap(array[j], array[min]);
+            swap(array[j], array[max]);
         }
     } else if (!strcmp(type, "Wilks")) {
         for (int j=0; j<size; ++j) {
-            min = j;
+            max = j;
             for (int k=j+1; k<size; ++k) {
-                if (array[k].wilksScore() > array[min].wilksScore()) {
-                    min = k;
+                if (array[k].wilksScore() > array[max].wilksScore()) {
+                    max = k;
                 }
             }
-            swap(array[j], array[min]);
+            swap(array[j], array[max]);
         }
     }
-
 }
 
+// Function XVI
+/***************************************************************
+Function: Swap
+
+Use: Helper function to facilitate the swap in the function above
+
+Arguments: two references to powerlifter objects
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void swap(PowerLifter& val1, PowerLifter& val2) {
     PowerLifter tmp = val1;
     val1=val2;
     val2=tmp;
 }
 
+// Function XVII
+/***************************************************************
+Function: readFile
+
+Use: Used to read the lifters.txt data sheet, and build our powerlifter array.
+
+Arguments: obj array and filename
+
+Returns: Integer for the count of insertertions
+
+Notes: 
+***************************************************************/
 int readFile( PowerLifter array[], const char fileName[] ) {
 
+    // Create our file object and open the file
     std::ifstream file(fileName);
 
+    // Variables to house the file contents
     char fname[NAME_MAXSIZE];
     char lname[NAME_MAXSIZE];
     float wilks;
     int squat, bench, deadlift, count=0;
 
 
+    // While we have data to read...
     while (file >> fname >> lname >> wilks >> squat >> bench >> deadlift) {
+        // Create a powerlifter object
         PowerLifter pn;
 
+        // Set the values for the object, get the total and wilks score, etc...
         pn.setFirst(fname);
         pn.setLast(lname);
         pn.setCoef(wilks);
@@ -284,19 +502,35 @@ int readFile( PowerLifter array[], const char fileName[] ) {
         pn.getTotal();
         pn.wilksScore();
 
+        // Append the object to the arary
         array[count] = pn;
 
+        // Update the count
         ++count;
-
     }
 
     return count;
 }
 
+// Function XVIII
+/***************************************************************
+Function: printPowerLifters
+
+Use: Prints the message that displays how many objects we created from the file contents, prints the header and column names, 
+then displays the values for each object
+
+Arguments: Powerlifter array, size of array, title for output
+
+Returns: void
+
+Notes: 
+***************************************************************/
 void printPowerLifters( PowerLifter array[], int size, const char title[] ) {
 
+    // Display the formatted title
     cout << std::setw(TITLE_JUST + strlen(title)) << title << endl;
 
+    // Simple loop to display each column name
     for (int i=0; i<N_HEAD; ++i) {
         if ( i < 2 ) {
             cout << std::right << std::setw(STRING_JUST) << HEADS[i];
@@ -306,13 +540,9 @@ void printPowerLifters( PowerLifter array[], int size, const char title[] ) {
     }
     LINE_BREAK
 
+    // Display the contents
     for (int i=0; i<size; ++i) {
         array[i].display();
     }
-
     cout << endl << endl;
-
-
 }
-
-
