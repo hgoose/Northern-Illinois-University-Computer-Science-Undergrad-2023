354a932b099c343b8edbe666a7677d3028bad244
hgoose <noodlecloudcode@gmail.com>
2024-08-07 13:17:02 -0500

    Commit

--- CS/Unix/Bash/exercises/bash/house/house.sh
+++ CS/Unix/Bash/exercises/bash/house/house.sh
@@ -1,98 +1,49 @@
 #!/usr/bin/env bash
-
-function main {
-
-    x="This is the house that Jack built.
-
-This is the malt
-that lay in the house that Jack built.
-
-This is the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the farmer sowing his corn
-that kept the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the horse and the hound and the horn
-that belonged to the farmer sowing his corn
-that kept the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built."
-
-echo $x
-
+initial=(
+  'This is the house that Jack built.'
+  'This is the malt'
+  'This is the rat'
+  'This is the cat'
+  'This is the dog'
+  'This is the cow with the crumpled horn'
+  'This is the maiden all forlorn'
+  'This is the man all tattered and torn'
+  'This is the priest all shaven and shorn'
+  'This is the rooster that crowed in the morn'
+  'This is the farmer sowing his corn'
+  'This is the horse and the hound and the horn'
+)
+refrain=(
+  'that lay in the house that Jack built.'
+  'that ate the malt'
+  'that killed the rat'
+  'that worried the cat'
+  'that tossed the dog'
+  'that milked the cow with the crumpled horn'
+  'that kissed the maiden all forlorn'
+  'that married the man all tattered and torn'
+  'that woke the priest all shaven and shorn'
+  'that kept the rooster that crowed in the morn'
+  'that belonged to the farmer sowing his corn'
+)
+(( $# == 2 )) || exit 1
+phrase () {
+  local -i i=$(($1 - 1))
+  printf '%s\n' "${initial[i]}"
+  for (( i-- ; i >= 0; i-- )); do
+    printf '%s\n' "${refrain[i]}"
+  done
+  printf '\n'
 }
-main "$@"
+house () {
+  if (( $1 > $2 )) || (( $1 < 1 || $2 < 1 )) || (( $1 > 12 || $2 > 12 )); then
+    echo invalid
+    return 1
+  fi
+  local -i i
+  for (( i = $1; i <= $2; i++ )); do
+    phrase "$i"
+  done
+}
+house "$@"
+
