eaec94761b0fd08798aa61892fd4a6501794da86
hgoose <noodlecloudcode@gmail.com>
2025-01-23 17:16:06 -0600

    Commit

--- CS/lang/julia/prog/1/main.jl
+++ CS/lang/julia/prog/1/main.jl
@@ -1,12 +1,2 @@
-a = [1,2,3]
-println(1 in a) # True
-println(5 in a) # False
-
-if 2 in a
-    println("2 is in a")
-end
-
-# Error: Use occursin
-b = "Julia"
-println("J" in b)
-println("x" in b)
+squared = (1:5) .^ 2
+println(squared)
