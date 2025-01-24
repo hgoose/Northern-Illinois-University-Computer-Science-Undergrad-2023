374d0e5ac359551e9fbcfa70361b2ace80833deb
hgoose <noodlecloudcode@gmail.com>
2025-01-24 15:56:43 -0600

    Commit

--- CS/lang/Cpp/formal-dsa/dsacc/hashing/unorderedmap/main.cc
+++ CS/lang/Cpp/formal-dsa/dsacc/hashing/unorderedmap/main.cc
@@ -19,21 +19,105 @@ private:
     vector<list<pair<key, value>>> buckets;
     size_t size = 0;
 
-    void hash(key k) {
+    size_t hash(const key& k) const {
         return std::hash<key>{}(k) % buckets.size();
     }
 
     void rehash() {
+        vector<list<pair<key,value>>> new_buckets(buckets.size() * 2);
 
+        for (const auto& bucket : buckets) {
+            for (const auto& pair : bucket) {
+                size_t h = std::hash<key>{}(pair.first) % (buckets.size() * 2);
+                new_buckets[h].emplace_back(pair.first, pair.second);
+            }
+        }
+        buckets.swap(new_buckets);
     }
 
 public:
-
     umap() : buckets(default_buckets) {}
 
+    void insert(const key& k, const value& v) {
+        if (static_cast<float>(size) / buckets.size() > load_factor) {
+            rehash();
+        } 
+
+        size_t h = hash(k);        
+        auto& bucket = buckets[h];
+
+        for (auto it = bucket.begin(); it!=bucket.end(); ++it) {
+            if (it->first == k) {
+                it->second = v;
+                return;
+            }
+        }
+        bucket.emplace_back(k,v);
+        ++size;
+    }
+
+    value* find(const key& k) {
+        size_t h = hash(k);
+        auto& bucket = buckets[h];
+
+        for (auto it = bucket.begin(); it!=bucket.end(); ++it) {
+            if (it->first == k) {
+                return &(it->second);
+            }
+        }
+        return nullptr;
+    }
+
+    bool empty() const {
+        return size == 0;
+    }
+
+    void erase(const key& k) {
+        size_t h = hash(k);
+        auto& bucket = buckets[h];
+
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->first == k) {
+                bucket.erase(it);
+                --size;
+                return;
+            }
+        }
+    }
+
+    value& operator[](const key& k) {
+        size_t h = hash(k);
+        auto& bucket = buckets[h];
+        for (auto it = bucket.begin(); it!=bucket.end(); ++it) {
+            if (it->first == k) return it->second;
+        }
+
+        buckets[h].emplace_back(k, value());
+        ++size;
+        return buckets[h].back().second;
+    }
+
+    void print() const {
+        for (const auto& bucket : buckets) {
+            for (const auto& pair : bucket) {
+                cout << pair.first << "->" << pair.second << endl;
+            }
+        }
+    }
 };
 
 int main(int argc, char** argv) {
+
+    umap<int,int> m;
+    m.insert(10,20);
+    m.insert(5,10);
+    m[1] = 10;
+    m[1] = 25;
+
+    
+
+    cout << endl << endl;
+    m.print();
 
     
 
