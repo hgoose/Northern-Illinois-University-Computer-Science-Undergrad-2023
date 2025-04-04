c3579b89c22dd14942c16ca0783e9c7033332de7
hgoose <noodlecloudcode@gmail.com>
2025-04-04 18:28:14 -0500

    Commit

--- CS/courses/CS-360/assignments/8/ASSIGN8.asm
+++ CS/courses/CS-360/assignments/8/ASSIGN8.asm
@@ -38,6 +38,13 @@ PAYROLL CSECT
          PACK  PFWHPCT(4),IFEDWITH(6)    Convert stwith to PD
          PACK  PSWHPCT(4),ISTWITH(6)     Convert Fedwith to PD
 *
+*
+*
+         LA    2,19
+         LA    3,19
+*
+* Start Loop
+*
          XREAD RECORD,80                 Beginning of read loop 
 LOOP1    BC    B'0111',ENDLOOP1          Branch if done
 *
@@ -46,10 +53,10 @@ LOOP1    BC    B'0111',ENDLOOP1          Branch if done
 *
 * Handle EmpID
 *
-         PACK  PEMPID(5),IEMPID(8)                 Convert EMPID to PD
-         SRP   PEMPID(5),1,0
-         MVC   OEMPID(10),=X'F0202060202020202020'
-         ED    OEMPID(10),PEMPID                  Move ID into Output
+         PACK  PEMPID(5),IEMPID(8)            Convert EMPID to PD
+         MVC   OEMPID-1(10),=X'21202060202020202020'
+         ED    OEMPID-1(10),PEMPID
+         MVI   DETAIL,C'0'
 *
 * Handle hourly pay
 *
@@ -63,8 +70,8 @@ LOOP1    BC    B'0111',ENDLOOP1          Branch if done
 * Handle Hours worked
 *
          PACK  PHOURS(3),IHOURS(5)
-         MVC   OHOURS(6),=X'402021204B2020'
-         ED    OHOURS(6),PHOURS
+         MVC   OHOURS(7),=X'402021204B2020'
+         ED    OHOURS(7),PHOURS
 *
 * Handle deduction amount 
 *
@@ -78,12 +85,93 @@ LOOP1    BC    B'0111',ENDLOOP1          Branch if done
 *
          MVC   OEMPNME(25),RECORD+28
 *
+* Handle Gross pay
+*
+         ZAP   PGRPAY(6),PHRPAY(3)
+         MP    PGRPAY(6),PHOURS(3)
+         SRP   PGRPAY(6),64-2,5
+         SP    PGRPAY(6),PDEDUCT(3)
+         AP    PGRPAY(6),PBONUS(3)
+         LA    1,OGRPAY+11                   Saftey mark for $
+         MVC   OGRPAY,=X'402020206B2020206B2021204B2020' Construct EP
+         EDMK  OGRPAY(15),PGRPAY            Edit gross into printline
+         BCTR  1,0                          Move R1 to $ position
+         MVI   0(1),C'$'                    Place $
+         AP    PTGRPAY(7),PGRPAY(6)
+*
+* Handle fed withholding amount
+*
+         ZAP   PCALC(10),PGRPAY(6)
+         MP    PCALC(10),PFWHPCT(4)
+         SRP   PCALC(10),64-5,5
+         ZAP   PFEDWITH(6),PCALC(10)
+         LA    1,OFEDWITH+11
+         MVC   OFEDWITH(15),=X'402020206B2020206B2021204B2020'
+         EDMK  OFEDWITH(15),PFEDWITH
+         BCTR  1,0
+         MVI   0(1),C'$'
+         AP    PTFWITH(7),PFEDWITH(6)
+*
+* Handle State withholding amount
+*
+         ZAP   PCALC(10),PGRPAY(6)
+         MP    PCALC(10),PSWHPCT(4)
+         SRP   PCALC(10),64-5,5
+         ZAP   PSTWITH(6),PCALC(10)
+         LA    1,OSTWITH+11
+         MVC   OSTWITH(15),=X'402020206B2020206B2021204B2020'
+         EDMK  OSTWITH(15),PSTWITH
+         BCTR  1,0
+         MVI   0(1),C'$'
+         AP    PTSWITH(7),PSTWITH(6)
+*
+* Handle Net Pay
+*
+         ZAP   PNETPAY(6),PGRPAY(6)     
+         SP    PNETPAY(6),PFEDWITH(6)
+         SP    PNETPAY(6),PSTWITH(6)
+         LA    1,ONETPAY+11
+         MVC   ONETPAY(15),=X'402020206B2020206B2021204B2020' 
+         EDMK  ONETPAY(15),PNETPAY
+         BCTR  1,0
+         MVI   0(1),C'$'
+         AP    PTNETPAY(7),PNETPAY(6)
+*
 * PRINT, READ, BRANCH  
 *
-         XPRNT DETAIL+1,133                   Print the line
+         CR    2,3
+         BC    B'0111',NOPRINT
+         LA    2,0
+         AP    PPAGECTR(2),=PL1'1'
+         MVC   OPAGENM(4),=X'40202120'
+         ED    OPAGENM(4),PPAGECTR
+         XPRNT ONMEPAGE,133
+         XPRNT OTITLE,133
+         XPRNT OHEAD1,133
+         XPRNT OHEAD2,133
+         XPRNT OHEAD3,133
+NOPRINT  DS    0H
+*
+         XPRNT DETAIL,133                   Print the line
+         AP    PEMPCTR(3),=PL1'1'
+         LA    2,1(,2)
          XREAD RECORD,80                    Read the next record
          BC    B'1111',LOOP1                Branch to check
 ENDLOOP1 DS    0H                           End of loop
+*
+* Print Totals page headers
+         AP    PPAGECTR(2),=PL1'1'
+         MVC   OPAGENM(4),=X'40202120'
+         ED    OPAGENM(4),PPAGECTR
+         XPRNT ONMEPAGE,133
+         XPRNT OTITLE,133
+         XPRNT OTOTALS,133
+*
+* Handle total number of employees
+*
+         MVC   OTEMPCT(6),=X'402020202120' 
+         ED    OTEMPCT(6),PEMPCTR
+         XPRNT OEMPCTR,133
 *
 * STANDARD EXIT LINKAGE WITH RC OF 0
 *
@@ -107,7 +195,7 @@ SAVEAREA DS 18F REGISTER SAVE AREA
 *
 IFEDWITH DS    ZL6
 ISTWITH  DS    ZL6
-FILL1    DS    CL68
+         DS    CL68
 * 
 * STORAGE FOR LOOPED XREAD
 *
@@ -146,35 +234,119 @@ PTSWITH  DC    PL7'0' PACKED TOTAL STATE WITHHOLDING
 PTNETPAY DC    PL7'0' PACKED TOTAL NET EMPLOYEE PAY
 *
 *
+* STORAGE FOR COMPANY NAME AND PAGE NUMBER
+*
+ONMEPAGE DC    C'1'
+         DC    52C' '
+         DC    C'CHICAGO SOFTWARE COMPANY, INC.'
+         DC    40C' '
+         DC    C'PAGE: '
+OPAGENM  DC    4C' '
+*
+* STORAGE FOR TITLE
+*
+OTITLE   DC    C' '
+         DC    54C' '
+         DC    C'EMPLOYEE PAYROLL REPORT' 
+         DC    55C' '
+*
+* Storage for column headers
+*
+OHEAD1   DC    C' '
+         DC    C'EMPLOYEE'
+         DC    C'   '
+         DC    C'EMPLOYEE'
+         DC    C'                     '
+         DC    C'HOURLY'
+         DC    C'     '
+         DC    C'HOURS'
+         DC    C'            '
+         DC    C'EMPLOYEE'
+         DC    C'    '
+         DC    C'EMPLOYEE FEDERAL'
+         DC    C'    '
+         DC    C'EMPLOYEE STATE'
+         DC    C'          '
+         DC    C'EMPLOYEE'
+OHEAD2   DC    C' '
+         DC    C'ID         '
+         DC    C'NAME                            '
+         DC    C'PAY    '
+         DC    C'WORKED           '
+         DC    C'GROSS PAY         '
+         DC    C'WITHHOLDING       '
+         DC    C'WITHHOLDING           '
+         DC    C'NET PAY'
+OHEAD3   DC    C' '
+         DC    C'---------  '
+         DC    C'-------------------------   '
+         DC    C'-------   '
+         DC    C'-------     '
+         DC    C'---------------    '
+         DC    C'----------------    '
+         DC    C'--------------   '
+         DC    C'---------------'
+*
+* Storage For TOTALS line
+*
+OTOTALS  DC    C' '
+         DC    62C' '
+         DC    C'TOTALS'
+         DC    64C' '
+*
+* Storage for number of employees 
+*
+OEMPCTR  DC    C'0'
+         DC    6C' '
+         DC    C'NUMBER OF EMPLOYEES:'
+         DC    11C' '
+OTEMPCT  DS    CL6
+         DC    90C' '
+*
+* Storage for total gross and average gross
+*
+OTGRPAY  DC    C'0'
+*
+* Storage for total fed withholding and average fed withholding
+*
+OTFWITH  DC    C'0'
+*
+* Storage for total state withholding and average state withholding
+*
+OTSWITH  DC    C'0'
+*
+* Storage for total net and average net
+*
+OTNETPAY  DC   C'0'
+*
 * STORAGE FOR MAIN PRINT LINE
 *
 DETAIL   DC    C'0'
 *
 OEMPID   DS    CL10
-         DC    2C' '
+         DC    C' '
 *
-OEMPNME  DS    CL25
-         DC    4C' '
+OEMPNME  DS    29C' '
 *
 OHRPAY   DS    CL7
-         DC    4C' '
+         DC    2C' '
 *
-OHOURS   DS    CL6
-         DC    11C' '
+OHOURS   DS    CL7
+         DC    2C' '
 *
-OGRRPAY  DS    CL8
-         DC    4C' '
+OGRPAY  DS    CL15
+         DC    2C' '
 *
-OFEDWITH DS    CL8
-         DC    4C' '
+OFEDWITH DS    CL15
+         DC    2C' '
 *
-OSTWITH  DS    CL8
-         DC    4C' '
+OSTWITH  DS    CL15
+         DC    2C' '
 *
-ONET     DS    CL8
-         DC    4C' '
+ONETPAY  DS    CL15
+         DC    2C' '
 *
-FILL2    DC    28C' '    
+FILL2    DC    8C' '    
          END PAYROLL
 /*
 //*
